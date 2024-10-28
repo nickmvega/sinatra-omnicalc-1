e805de81ccbba17d2c5745dc02414699d2fcfb4b
Nicholas Vega <122232969+nickmvega@users.noreply.github.com>
2024-10-28 20:26:42 +0000

    omni hw

--- app.rb
+++ app.rb
@@ -1,9 +1,55 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/square/new") do
+  erb(:square_form)
 end
+
+get("/square/results") do
+  @number = params["number"].to_f
+  @square = @number ** 2
+  erb(:square_form_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do 
+  @user_number = params["user_number"].to_f
+  @square_root = @user_number ** 0.5
+  erb(:square_root_results)
+end
+
+get("/payment/new") do 
+  erb(:payment_form)
+end
+
+get("/payment/results") do
+  apr = params["user_apr"].to_f
+  years = params["user_years"].to_i
+  principal = params["user_pv"].to_f
+
+  r = apr / 100 / 12
+
+  n = years * 12
+
+  @apr = apr
+  @years = years
+  @principal = principal
+
+  @payment = r * principal / (1 - (1 + r)**-n)
+
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random_form)
+end
+
+get("/random/results") do
+  @min = params["user_min"].to_f
+  @max = params["user_max"].to_f
+  @random = rand(@min...@max) 
+  erb(:random_results)
+end 
